be8b4b3e3d2053af821636b2baaec702628c54f5
Abdul-Ahad <121967743+ahad324@users.noreply.github.com>
2023-03-28 21:27:06 +0500

    Add files via upload

--- webcount.js
+++ webcount.js
@@ -1,4 +1,4 @@
-var countdowndate = new Date("Mar 1,2023 00:00:00").getTime();
+var countdowndate = new Date("April 3,2023 00:00:00").getTime();
 var x = setInterval(function () {
     var now = new Date().getTime();
     var distance = countdowndate - now;
